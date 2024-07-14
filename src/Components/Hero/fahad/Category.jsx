dc0ac9d94af6c332e8b419bcd67857e3976ec98e
DeveloperFahad <developerfahad.me@gmail.com>
2024-07-15 02:48:11 +0600

    second paragraph

--- src/Components/Hero/fahad/Category.jsx
+++ src/Components/Hero/fahad/Category.jsx
@@ -6,6 +6,7 @@ const Category = () => {
             <h1>This is category page.</h1>
             <h2>This is a subtitle.</h2>
             <p>This is a paragraph</p>
+            <p>This is a second paragraph.</p>
         </div>
     );
 };
